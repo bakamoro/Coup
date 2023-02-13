6f9b3159eff144c5538be1e804661f7bb5f4f859
Bakamoro <binyaminkenig@gmail.com>
2023-02-13 20:18:45 +0200

    arrange create game case

--- app/src/main/java/com/example/coup/createGame.java
+++ app/src/main/java/com/example/coup/createGame.java
@@ -1,13 +1,21 @@
 package com.example.coup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class createGame extends AppCompatActivity {
     EditText textView;
@@ -22,13 +30,56 @@ public class createGame extends AppCompatActivity {
     public void createGame(View view) {
         game_name = textView.getText().toString();
         if(game_name!= null && !game_name.equals("")) {
-//            FireStoreHelper fireStoreHelper = new FireStoreHelper(game_name, getApplicationContext());
-//            fireStoreHelper.startFireStore(4);
-            Toast.makeText(this,"game created",Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(createGame.this,gameActivity.class);
-            intent.putExtra("game_name",game_name);
-            startActivity(intent);
+            isGameExist();
         }
         else Toast.makeText(this,"please enter game's name",Toast.LENGTH_SHORT).show();
+    }
+
+    private void isGameExist() {
+        TextView textView = findViewById(R.id.gameNameText);
+        String game_name = textView.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("chess games").document(game_name);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Toast.makeText(createGame.this, "game is already exist", Toast.LENGTH_SHORT).show();
+
+                        //TODO: להזיז את הקוד לjoin
+//                        if((int)document.get("number of player") < 4) {
+//                            if(document.get("number of player") == null){
+//                                moveToGame(1);
+//                            }
+//                            moveToGame((int)document.get("number of player")+1);
+//                        }
+//                        else Toast.makeText(createGame.this,"game is full",Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        moveToGame(1);
+                    }
+                } else {
+//                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void moveToGame(int number_of_players) {
+        FireStoreHelper fireStoreHelper = new FireStoreHelper(game_name, getApplicationContext());
+        Game game = new Game();
+        gameActivity gameActivity = new gameActivity();
+        gameActivity.setGame(game);
+        fireStoreHelper.startFireStore(4, game);
+
+        Toast.makeText(this, "game created", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(createGame.this, waitingRoom.class);
+        intent.putExtra("game_name", game_name);
+        intent.putExtra("number of players", number_of_players);
+        startActivity(intent);
+        finish();
     }
 }
