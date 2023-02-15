92540be48e891beaf8e2a1e75466003b17e91219
Bakamoro <binyaminkenig@gmail.com>
2023-02-15 19:01:28 +0200

    add lastAction field in game

--- app/src/main/java/com/example/coup/Game.java
+++ app/src/main/java/com/example/coup/Game.java
@@ -6,6 +6,7 @@ public class Game {
     private Player player3;
     private Player player4;
     private Deck deck;
+    private String lastAction;
     private int num_of_players,require_player_num;
     public Game(Player player1, Player player2, Player player3, Player player4, Deck deck){
         this.player1 = player1;
@@ -62,5 +63,13 @@ public class Game {
             }
         }
         return player1;
+    }
+
+    public String getLastAction() {
+        return lastAction;
+    }
+
+    public void setLastAction(String lastAction) {
+        this.lastAction = lastAction;
     }
 }
