1a017f8504b9b4f871a9d9694a8a9fc35e3c8b5a
Bakamoro <binyaminkenig@gmail.com>
2023-02-19 18:38:08 +0200

    filling attack()

--- app/src/main/java/com/example/coup/Captain.java
+++ app/src/main/java/com/example/coup/Captain.java
@@ -12,7 +12,14 @@ public class Captain extends Card{
     @Override
     public void attack(Game game,int myPlayerNumber,int victimPlayerNumber){
         game.setLastAction(Actions.captain_steal_2_coins);
-
+        if(game.getPlayer(myPlayerNumber).getPersonalBank().getCoins().getNumber()>1){
+            game.getPlayer(myPlayerNumber).getPersonalBank().getCoins().addCoins(2);
+            game.getPlayer(victimPlayerNumber).getPersonalBank().getCoins().reduceCoins(2);
+        }
+        else if(game.getPlayer(myPlayerNumber).getPersonalBank().getCoins().getNumber()==1){
+            game.getPlayer(myPlayerNumber).getPersonalBank().getCoins().addCoins(1);
+            game.getPlayer(victimPlayerNumber).getPersonalBank().getCoins().reduceCoins(1);
+        }
     }
     @Override
     public void defend(Game game,int myPlayerNumber,int victimPlayerNumber){
